7982f35cf05876b4ce4457d7b35df6a8233fe916
Michael Q Larson <michaelqlarson@gmail.com>
2014-09-14 14:40:12 -0700

    finish skelliton of app

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   get '/signin' => 'sessions#new', :as => :signin
   get '/signout' => 'sessions#destroy', :as => :signout
   get '/auth/failure' => 'sessions#failure'
+  get 'page_two' => 'visitors#page_two'
+  get 'page_three' => 'visitors#page_three'
+  get 'page_four' => 'visitors#page_four'
+  get 'page_five' => 'visitors#page_five'
 end
